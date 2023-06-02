66f6b130f39c21efb3ff8d73f105d6d8da687446
Asiya <asiya_ermolaeva@mail.ru>
2023-06-02 18:30:43 +0300

    add Register and Login handle

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from '../../img/logo.png'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../img/logo.png';
 import styles from './Navbar.module.scss';
+import { AuthContext } from '../../context/authContext';
 
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
-	<div className={styles.navbar}>
-	  <div className={styles.container}>
-      <div className={styles.logo}>
-        <img src={Logo} alt='logo'/>
-      </div>
+    <div className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src={Logo} alt="logo" />
+        </div>
 
-      <div className={styles.links}>
-      <Link className='link' to='/?cat=art'><h6>art</h6></Link>
-      <Link className='link' to='/?cat=science'><h6>science</h6></Link>
-      <Link className='link' to='/?cat=technology'><h6>technology</h6></Link>
-      <Link className='link' to='/?cat=design'><h6>design</h6></Link>
-      <span>John</span>
-      <span>Logout</span>
-      <span className={styles.write}><Link className='link' to='/write'>Write</Link></span>
-
+        <div className={styles.links}>
+          <Link className="link" to="/?cat=art">
+            <h6>art</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>science</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>technology</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>design</h6>
+          </Link>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className={styles.write}>
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
